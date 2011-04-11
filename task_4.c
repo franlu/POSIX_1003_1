110685ae5e38d082959fa971de0b635da57ec1ff
Fran Lucena <fran@osl-02.(none)>
2011-04-11 10:18:01 +0200

    filter arguments

--- task_4.c
+++ task_4.c
@@ -2,3 +2,24 @@
 task_4.c
 Standard management functions Directory ''POSIX 2.10 compliant''
 */
+
+#include<sys/types.h>  	//Primitive system data types for abstraction of implementation-dependent data types.
+						//POSIX Standard: 2.6 Primitive System Data Types <sys/types.h>
+#include<sys/stat.h>
+#include<fcntl.h>
+#include<stdlib.h>
+#include<stdio.h>
+#include<errno.h>
+
+#include<dirent.h>
+
+
+int main(int argc, char *argv[]){
+	
+if(argc<2) {
+	printf("\nSintaxis de ejecucion: tarea4 [<pathname>]+ [<4digit_octal_number>]+ \n\n");
+	exit(-1);
+}	
+
+return 0;
+}
