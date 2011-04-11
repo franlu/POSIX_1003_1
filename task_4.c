773678eb6a8a6dc72d81a7dfa41a897ffbb9d77e
Fran Lucena <fran@osl-02.(none)>
2011-04-11 11:35:51 +0200

    open and read directory

--- task_4.c
+++ task_4.c
@@ -15,11 +15,44 @@ Standard management functions Directory ''POSIX 2.10 compliant''
 
 
 int main(int argc, char *argv[]){
+
+DIR *directorio;
+struct dirent *file;
+int fd;
+struct stat atributos;
+
+	if(argc<2) {
+		printf("\nSintaxis de ejecucion: tarea4 [<pathname>]+ [<4digit_octal_number>]+ \n\n");
+		exit(-1);
+	}	
+
+	directorio = opendir(argv[1]);
+
+	file = readdir(directorio);
+	while(file != NULL){
+			printf("\nFile Name: %s",file->d_name);
+			
+			if( (fd=open(file->d_name,O_RDONLY,S_IRUSR))<0) {
+				//printf("\nError %d en open",errno);
+				perror("\nOpen Fail\n.");
+				//exit(-1);
+			}
+		
+			if(stat(file->d_name,&atributos) < 0) {
+				//printf("\nError al intentar acceder a los atributos");
+				//perror("\nstat Fail");
+				//exit(-1);
+			}
+			close(fd);
+			
+			file = readdir(directorio);
+	}
 	
-if(argc<2) {
-	printf("\nSintaxis de ejecucion: tarea4 [<pathname>]+ [<4digit_octal_number>]+ \n\n");
-	exit(-1);
-}	
+	if(closedir(directorio)<0) {
+		printf("\nclosedir Fail \n\n");
+		exit(-1);
+	}
+	
 
 return 0;
 }
