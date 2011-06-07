c1ac7f6bf33a75ace33051323ce1e29d86e36ec8
Fran Lucena <fran.lucena@gmail.com>
2011-06-07 21:59:01 +0200

    there is an error when running the parent process

--- task_09_a.c
+++ task_09_a.c
@@ -13,32 +13,35 @@ Working with system calls Process Subsystem POSIX 2.10
 #include<stdlib.h>
 
 
-int main(int argc, char *argv[])
-{
+
+int main(int argc, char *argv[]){
+
+int num_hijos = 2;
+int i,estado;
+pid_t pid,pida;
+char aux[20];
 
 
-int num_hijos = 5;
-pid_t hijos[num_hijos];
-
-
-for(int i=0;i<num_hijos;i++)
-    if ( (hijos[i] = fork()) < 0){
-        perror("\nError at fork call number %d ",i);
+for(i=0;i<num_hijos;i++)
+    if ( (pid = fork()) < 0){
+        perror("\nError at fork call");
 	exit(-1);
-    } else {
-        
     }
     
 if(pid==0) {  //child process run the program
-    sprintf(aux,"\nI am the son %d:\n",hijos[i]);
-        if  (write(STDOUT_FILENO,aux,sizeof(aux)+1) != sizeof(aux)+1) {
-            perror("\nError writing to STDOUT");
-            exit(-1);
-        }
-    //call for end child process
+    
+    sleep(1);
+    printf("\nI am the son %d \n",getpid());
+    
 }
-		
-
+else {
+   
+   for(i=0;i<num_hijos;i++){
+	pida =  wait(&estado);
+	printf("\nMi hijo %d ha finalizado con estado %d",pida,estado);
+	printf("\nTengo %d hijos vivos.",num_hijos-i);
+    }
+  }
 
 
 exit(0);
