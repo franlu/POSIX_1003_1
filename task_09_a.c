e7d5cfe32671337d88319ed927724320d82849c1
Fran Lucena <fran.lucena@gmail.com>
2011-06-10 22:34:56 +0200

    Error not fixed

--- task_09_a.c
+++ task_09_a.c
@@ -5,6 +5,7 @@ Working with system calls Process Subsystem POSIX 2.10
     
 */
 
+
 #include<sys/types.h> //Primitive system data types for abstraction of implementation-dependent data types.
                       //POSIX Standard: 2.6 Primitive System Data Types <sys/types.h>
 #include<unistd.h>    //POSIX Standard: 2.10 Symbolic Constants         <unistd.h>
@@ -13,36 +14,28 @@ Working with system calls Process Subsystem POSIX 2.10
 #include<stdlib.h>
 
 
-
 int main(int argc, char *argv[]){
 
-int num_hijos = 2;
-int i,estado;
-pid_t pid,pida;
-char aux[20];
-
-
-for(i=0;i<num_hijos;i++)
-    if ( (pid = fork()) < 0){
-        perror("\nError at fork call");
-	exit(-1);
+int num_hijos = 3;
+int i, estado;
+pid_t pid;
+  
+for(i=0; i<num_hijos; i++)
+    if((pid = fork())<0){
+      perror("Error fork\n");
+      exit(-1);
     }
     
-if(pid==0) {  //child process run the program
-    
-    sleep(1);
-    printf("\nI am the son %d \n",getpid());
-    
+if(pid==0){
+	printf("I am the son %i\n", getpid());
+	//exit(0);
+    }
+
+for(i=num_hijos-1; i>=0; i--){
+    pid = wait(&estado);
+    printf("My son just finished with PID = %i\n", pid);
+    printf("I only have %i living children\n", i);
 }
-else {
-   
-   for(i=0;i<num_hijos;i++){
-	pida =  wait(&estado);
-	printf("\nMi hijo %d ha finalizado con estado %d",pida,estado);
-	printf("\nTengo %d hijos vivos.",num_hijos-i);
-    }
-  }
-
 
 exit(0);
 
